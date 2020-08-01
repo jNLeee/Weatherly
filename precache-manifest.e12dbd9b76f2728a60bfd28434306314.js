3f389136cb520567c62c6b84229a988ab4e9da4b
jNLeee <jlee232435@gmail.com>
2020-07-31 20:38:28 -0500

    Updates

---
+++ precache-manifest.e12dbd9b76f2728a60bfd28434306314.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb08e036969afa2c270e5a8032f30208",
+    "url": "/Weatherly/index.html"
+  },
+  {
+    "revision": "5cefd2354dec8a65c51e",
+    "url": "/Weatherly/static/css/main.0dca9383.chunk.css"
+  },
+  {
+    "revision": "fb57cbf26e92a6952548",
+    "url": "/Weatherly/static/js/2.928cbdac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weatherly/static/js/2.928cbdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cefd2354dec8a65c51e",
+    "url": "/Weatherly/static/js/main.be285603.chunk.js"
+  },
+  {
+    "revision": "0c4bf5d14a247eea15ef",
+    "url": "/Weatherly/static/js/runtime-main.6614e5c0.js"
+  },
+  {
+    "revision": "f4cd1fded32950551ecbfcfe5f8e1343",
+    "url": "/Weatherly/static/media/Breezy-bg.f4cd1fde.jpg"
+  },
+  {
+    "revision": "4f810ecfb987fd9015526c80ab502273",
+    "url": "/Weatherly/static/media/Chilly-bg.4f810ecf.jpg"
+  },
+  {
+    "revision": "17ebc1e3669524302dfcbf7e78f92533",
+    "url": "/Weatherly/static/media/Cold-bg.17ebc1e3.jpg"
+  },
+  {
+    "revision": "83b3dbecfb5de0445a9b2c0961cb787c",
+    "url": "/Weatherly/static/media/Hot-bg.83b3dbec.jpg"
+  },
+  {
+    "revision": "9ed35e3b7efae856b021350e4ada2093",
+    "url": "/Weatherly/static/media/Warm-bg.9ed35e3b.jpg"
+  }
+]);
